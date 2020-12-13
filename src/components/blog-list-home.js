12668589036507ef03971c31b3a9c8cadeeeba91
Marta <marta.nunes.abreu@gmail.com>
2020-12-13 16:44:49 +0000

    edited blog list layout

--- src/components/blog-list-home.js
+++ src/components/blog-list-home.js
@@ -7,7 +7,7 @@ import PostCard from "./post-card"
 const PostMaker = ({ data }) => (
   <section className="home-posts">
     <h2>Latest in <strong>Blog</strong> <span class="icon -right"><RiArrowDownLine/></span></h2>
-    <div className="grids col-1 sm-2 lg-3">
+    <div className="grids col-1 sm-1 lg-1">
       {data}
     </div>
     <Link className="button" to="/blog">See more<span class="icon -right"><RiArrowRightSLine/></span></Link>
